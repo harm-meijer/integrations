5ecf00d084beaee685e8244e3da875565e78776d
Harm Meijer <harm.meijer@commercetools.de>
2020-02-08 14:01:59 +0100

    for heroku

---
+++ secret.js
@@ -0,0 +1,9 @@
+const secret = {
+  PROJECT_KEY:process.env.PROJECT_KEY,
+  CLIENT_SECRET:process.env.CLIENT_SECRET,
+  CLIENT_ID:process.env.CLIENT_ID,
+  AUTH_URL:process.env.AUTH_URL,
+  API_URL:process.env.API_URL,
+  SCOPES:process.env.SCOPE,
+};
+module.exports = secret;
