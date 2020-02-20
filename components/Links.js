6983b6fd8caf81cc3f4dcb389a18c64b1f0ba889
Ivo Bronsveld <ivo.bronsveld@commercetools.com>
2020-02-20 09:47:13 +0100

    Changed the Links to look at product variant assets

--- components/Links.js
+++ components/Links.js
@@ -18,16 +18,19 @@ const Links = links => (
   </React.Fragment>
 )
 export default function LinksContainer({ product }) {
+
+  // The links will be created based on the assets
+
   return useMemo(() => {
-    const links = !product
+    const links = !product.assets
       ? []
-      : [
-          ['Download', 'Download'],
-          ['Documentation', 'documentation'],
-          ['Demo', 'demo']
-        ]
-          .map(([txt, key]) => [txt, product[key]])
-          .filter(([, href]) => href)
+      : product.assets.map((asset) => {
+
+        if (asset.name && asset.sources[0]) {
+          return [asset.name['en'], asset.sources[0].uri];
+        }
+
+      })
     return !links.length ? '' : Links(links)
   }, [product])
 }
