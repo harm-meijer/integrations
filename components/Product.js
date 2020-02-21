c13040b8530da26b381bda51a63a0b78a51ffd27
Harm Meijer <harm.meijer@commercetools.de>
2020-02-21 14:17:12 +0100

    Clickable product

--- components/Product.js
+++ components/Product.js
@@ -1,33 +1,49 @@
 import React from 'react'
 import Link from 'next/link'
-import Row from 'react-bootstrap/Row'
-import {Card, Text, Grid} from '@commercetools-frontend/ui-kit';
+import { Card, Text } from '@commercetools-frontend/ui-kit'
 
 const Product = ({
-                   logo,
-                   ShortDescription,
-                   slug,
-                   Vendor,
-                   name
-                 }) => (
-
-    <Card theme="light" type="raised">
-          <div className="image-row" style={{height: '75px', paddingLeft: '10px'}}>
-            <div>
-              <img src={logo} className="vendor-logo"/>
-            </div>
+  logo,
+  ShortDescription,
+  slug,
+  Vendor
+}) => (
+  <Card
+    theme="light"
+    type="raised"
+    className="product-list-item"
+  >
+    <Link
+      href={`/integration?slug=${slug}`}
+      as={`/integration/${slug}`}
+    >
+      <div>
+        <div
+          className="image-row"
+          style={{ height: '75px', paddingLeft: '10px' }}
+        >
+          <div>
+            <img src={logo} className="vendor-logo" />
           </div>
-          <hr/>
-          <div style={{height:'100px', overflow: 'hidden'}} className="line-clamp">
-
-            {ShortDescription}
-
-          </div>
-          <hr/>
-          <div style={{height: '30px', paddingLeft: '10px'}}>
-            <Text.Detail tone={'primary'}>{Vendor}</Text.Detail>
-          </div>
-    </Card>
+        </div>
+        <hr />
+        <div
+          style={{ height: '100px', overflow: 'hidden' }}
+          className="line-clamp"
+        >
+          {ShortDescription}
+        </div>
+        <hr />
+        <div
+          style={{ height: '30px', paddingLeft: '10px' }}
+        >
+          <Text.Detail tone={'primary'}>
+            {Vendor}
+          </Text.Detail>
+        </div>
+      </div>
+    </Link>
+  </Card>
 )
 
 export default Product
