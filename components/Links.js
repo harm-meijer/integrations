0aa8feb23c83b3ae639fc54b83efa377cfc1368b
Ivo Bronsveld <ivo.bronsveld@commercetools.com>
2020-02-24 14:23:04 +0100

    Updated the links to use Assets and use correct styling based on the tags set on the asset

--- components/Links.js
+++ components/Links.js
@@ -1,19 +1,44 @@
 import React, { useMemo } from 'react'
-import {PrimaryButton} from '@commercetools-frontend/ui-kit';
+import Link from 'next/link'
+import {PrimaryButton, SecondaryButton} from '@commercetools-frontend/ui-kit';
 
 const Links = links => (
   <React.Fragment>
-    {links.map(([txt, href]) => (
-      <PrimaryButton
-        tone="primary"
-        key={txt}
-        label={txt}
-        size="big"
-        buttonAttributes={{href:href, rel:'noopener noreferrer', target:'_blank'}}
-        as="a"
-      >
-      </PrimaryButton>
-    ))}
+    {links.map(([txt, href, tags]) => {
+
+        if (tags.indexOf('cta') > -1) {
+          return (
+            <PrimaryButton
+              tone="primary"
+              key={txt}
+              label={txt}
+              size="big"
+              as="button"
+              data-tag={JSON.stringify(tags)}
+              onClick={() => {
+                // route to new page by changing window.location
+                window.open(href, "_blank", "noopener") //to open new page
+              }}
+            >
+            </PrimaryButton>
+          )
+        } else {
+          return (
+
+            <SecondaryButton
+              theme="default"
+              key={txt}
+              label={txt}
+              onClick={() => {
+                // route to new page by changing window.location
+                window.open(href, "_blank", "noopener") //to open new page
+              }}
+            >
+            </SecondaryButton>
+          )
+        }
+      }
+    )}
   </React.Fragment>
 )
 export default function LinksContainer({ product }) {
@@ -26,7 +51,7 @@ export default function LinksContainer({ product }) {
       : product.assets.map((asset) => {
 
         if (asset.name && asset.sources[0]) {
-          return [asset.name['en'], asset.sources[0].uri];
+          return [asset.name['en'], asset.sources[0].uri, asset.tags];
         }
 
       })
