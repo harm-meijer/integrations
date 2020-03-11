8979b59b3831f81e76dd62e84d31a37094610d87
Harm Meijer <harm.meijer@commercetools.de>
2020-03-11 18:52:27 +0100

    try localhost

--- constants.js
+++ constants.js
@@ -1,7 +1,10 @@
 export const LANGUAGE = process.env.LANGUAGE
 export const API =
   process.env.NODE_ENV === 'production'
-    ? 'https://marketplace.commercetools.com/api'
+    ? // ? 'https://marketplace.commercetools.com/api'
+      //  can also try loopback ip
+      //probaly won't work but worth a try
+      'http://localhost/api'
     : 'http://localhost:8080/api'
 export const SITE =
   process.env.NODE_ENV === 'production'
