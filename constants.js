023699dd6be64b419d402f53496ee9752c312122
Harm Meijer <harm.meijer@commercetools.de>
2020-03-11 18:46:11 +0100

    setting correct remote url

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 export const LANGUAGE = process.env.LANGUAGE
 export const API =
   process.env.NODE_ENV === 'production'
-    ? 'https://integrations-sigma.now.sh/api'
+    ? 'https://marketplace.commercetools.com/api'
     : 'http://localhost:8080/api'
 export const SITE =
   process.env.NODE_ENV === 'production'
-    ? 'https://integrations-sigma.now.sh/'
+    ? 'https://marketplace.commercetools.com/'
     : 'http://localhost:8080/'
